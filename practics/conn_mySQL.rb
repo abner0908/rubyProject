a77bafde31249781937377419b67c70dd093b40e
abner0908 <abner0908@gmail.com>
2015-05-22 10:46:05 +0800

    alter conn_mySQL.rb

--- practics/conn_mySQL.rb
+++ practics/conn_mySQL.rb
@@ -1,13 +1,28 @@
 require 'mysql2'
 
 client = Mysql2::Client.new(
-    host: "localhost",
-    username: "abner0908",
-    password: "510241",
-    database: "cmdev",
+host: "192.168.1.18",
+username: "abner0908",
+password: "510241",
+database: "op_schd"
 )
-results  = client.query("select * from dept")
+client.query('begin')
 
-results.each { |row| puts row['dname'] }
+begin
 
+  (1..10).each do |no|
+    sql = "insert into time_log(tl_datetime, tl_remark) values(now(), #{no.to_s})"
+    client.query(sql)
+    sleep(1)
+  end
+
+rescue Exception => e
+  puts "Inserting data into time_log fail!! DB rollback!!"
+  puts e.to_s
+  client.query('rollback')
+  exit
+end
+
+client.query('commit')
+puts 'Inserting data into time_log success!!'
 client.close if client
